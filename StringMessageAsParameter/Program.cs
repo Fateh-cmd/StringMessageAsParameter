10502ca7637baa95436da4a35e917e65d6febeab
fateh bouretoua <bouretoua_fateh@hotmail.com>
2023-09-22 23:47:03 +0200

    Add StringMessageAsParameter

---
+++ StringMessageAsParameter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static string GetUserInput(string message)
+    {
+        Console.WriteLine(message);
+        string text = Console.ReadLine();
+        return text;
+    }
+
+    static void Main()
+    {
+        string message = "Please enter a number";
+        string enteredText = GetUserInput(message);
+        Console.WriteLine($"You entered: {enteredText}");
+    }
+}
